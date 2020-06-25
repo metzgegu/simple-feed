6e6b49563a748b050432444c20811e747fe40c3e
METZGER Guillaume <guillaume.metzger5@gmail.com>
2020-06-25 10:43:28 +0200

    Remove useless line in backend index

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Origin', '*');
   next();
 })
 
